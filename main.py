a8c0fbff845aa2ff4b8c30da7c25cb30fb538612
Mr-Leshiy <leshiy12345678@gmail.com>
2023-07-18 16:42:50 +0300

    add new files_url field, upload f10_open_source, f10_external_link, f10_external_link_2, f10_external_link_3

--- main.py
+++ main.py
@@ -196,6 +196,8 @@ def get_proposals(
             response = ideascale_get(url, api_token)
             for idx, idea in enumerate(response):
                 challenge = find_challenge(idea['campaignId'], challenges)
+                if not challenge:
+                    continue
                 temp_idea = extract_custom_fields(idea, relevant_keys)
                 parsed_idea = {
                     "category_name": f"Fund {fund_id}",
@@ -208,7 +210,13 @@ def get_proposals(
                     "proposal_impact_score": extract_score(idea["id"], assessments),
                     "proposal_summary": strip_tags(idea["text"]),
                     "proposal_title": strip_tags(idea["title"]),
-                    "proposal_url": idea["url"]
+                    "proposal_url": idea["url"],
+                    "files_url": {
+                            "open_source": idea["customFieldsByKey"]["f10_open_source"],
+                            "external_link1": idea["customFieldsByKey"]["f10_external_link"],
+                            "external_link2": idea["customFieldsByKey"]["f10_external_link_2"],
+                            "external_link3": idea["customFieldsByKey"]["f10_external_link_3"],
+                        },
                 }
                 if authors_output == 'std' or authors_output == 'merged_str':
                     proposers_name = extract_proposers(idea, authors_output)
